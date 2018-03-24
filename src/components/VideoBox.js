b508c1281266d52cc415496f5826af936b43eab5
locphan2207 <locphan2207@gmail.com>
2018-03-24 16:21:24 -0700

    add draggable component

--- src/components/VideoBox.js
+++ src/components/VideoBox.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import Draggable from 'react-draggable';
 
 export default class VideoBox extends React.Component {
   render() {
     return(
-      <div>
-        <div className="main-video-box">
+      <div className="video-box">
+        <div className="main-video">
           Main Stream Video
         </div>
-        <ul className="popup-video-container">
-          <li className="popup-video">Pop up video</li>
-          <li className="popup-video">Pop up video</li>
-          <li className="popup-video">Pop up video</li>
-        </ul>
+        <Draggable bounds="parent">
+          <div className="popup-video">
+            Pop up video
+          </div>
+        </Draggable>
       </div>
     );
   }
