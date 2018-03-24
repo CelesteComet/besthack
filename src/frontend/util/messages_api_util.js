267045505485b8ef7938cea37fbb636bd04feaa1
Alvin James Delos Santos <a.jamesdelossantos@gmail.com>
2018-03-24 16:20:39 -0700

    Add message api utils and dispatch actions

---
+++ src/frontend/util/messages_api_util.js
@@ -0,0 +1,38 @@
+export const fetchMessages = () => 
+  $.ajax({
+    url: "https://data.absolve11.hasura-app.io/v1/query",
+    contentType: "application/json",
+    data: JSON.stringify({
+      "type": "select",
+      "args": {
+        "table": "messages",
+          "columns": [
+            "*"
+          ]
+        }
+      }),
+      type: "GET",
+      dataType: "json"
+  });
+
+
+export const createMessage = (authorName, body) => 
+  $.ajax({
+    url: "https://data.absolve11.hasura-app.io/v1/query",
+    contentType: "application/json",
+    data: JSON.stringify({
+      "type": "insert",
+      "args": {
+        "table": "messages",
+        "objects": [
+            {
+              "author_name": `${authorName}`,
+              "body": `${body}`
+            }
+        ]
+      }
+    }),
+    type: "POST",
+    dataType: "json"
+  });
+
