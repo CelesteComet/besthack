76d28c4cb6714600aed6c0cc9651536718291e2e
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2018-03-24 16:59:35 -0700

    fix hello world lambda

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,15 @@
+// import keys from '../keys';
+
+// var API_KEY = keys['TOKBOX_API_KEY'];
+// var SECRET = keys['TOKBOX_SECRET'];
+
+// var OpenTok = require('opentok'),
+//     opentok = new OpenTok(API_KEY, SECRET);
+
 export function handler(event, context, callback) {
-  console.log(event)
+
   callback(null, {
     statusCode: 200,
-    body: JSON.stringify({msg: "Hello, EVERYONE AT GITHUB!"})
+    body: JSON.stringify({msg: "Hello, EVERYONE AT"})
   })
 }
