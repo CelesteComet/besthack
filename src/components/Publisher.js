bb144c75db8d00b32e1314623bc460bfb579a1ac
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2018-03-24 17:38:11 -0700

    have main video with popup

---
+++ src/components/Publisher.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { OTSession, OTPublisher, OTStreams, OTSubscriber, createSession} from 'opentok-react';
+
+class Publisher extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { streams: [] };
+
+  }
+
+  componentWillMount() {
+    this.sessionHelper = createSession({
+      apiKey: '46086882',
+      sessionId: '2_MX40NjA4Njg4Mn5-MTUyMTkyNjUwMjA2MX5FL1JpeDdubzFqVnhXMG0zOGV2cmUyTDZ-fg',
+      token: 'T1==cGFydG5lcl9pZD00NjA4Njg4MiZzaWc9Yzk4OTZiZDdmMDViMWNjNDViYjc1ZTk1YzU5MTYzMDE1YjU0YjBmYjpzZXNzaW9uX2lkPTJfTVg0ME5qQTROamc0TW41LU1UVXlNVGt5TmpVd01qQTJNWDVGTDFKcGVEZHViekZxVm5oWE1HMHpPR1YyY21VeVREWi1mZyZjcmVhdGVfdGltZT0xNTIxOTMxMzUxJm5vbmNlPTAuMjg4OTgwNzE3MDc3NDYyMSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTI0NTIzMzQ5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
+      onStreamsUpdated: streams => { this.setState({ streams }); }
+    });
+
+    this.properties = {
+      name: 'Bruce'
+    };
+  }
+ 
+  componentWillUnmount() {
+    this.sessionHelper.disconnect();
+  }
+
+  render() {
+    return (
+      <div>
+        <OTPublisher session={this.sessionHelper.session} properties={this.properties} />
+      </div>
+    );
+  }
+}
+
+export default Publisher;
