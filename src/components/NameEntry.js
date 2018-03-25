0820b62c76917706d223f753c4d687b18ce17858
Jack Wu <jackwu350@yahoo.com>
2018-03-25 00:57:57 -0700

    change img link to be https

--- src/components/NameEntry.js
+++ src/components/NameEntry.js
@@ -37,7 +37,7 @@ class NameEntry extends Component {
         <br/>
 
         <div className="townhall-about">
-          <img src="http://res.cloudinary.com/dbtepon6n/image/upload/c_scale,w_189/v1521964232/TownHall.svg" alt="main-logo" />
+          <img src="https://res.cloudinary.com/dbtepon6n/image/upload/c_scale,w_189/v1521964232/TownHall.svg" alt="main-logo" />
           <p> TownHall is a multi-purpose video streaming application. </p>
         </div>
 
