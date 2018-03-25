6d32722656e70d285e6eddefc947264bb58c4e21
Rainmire <mqyin@g.ucla.edu>
2018-03-24 22:15:31 -0700

    fix OT

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -1,32 +1,8 @@
 import React from 'react';
-import { OTSession, OTPublisher, OTStreams, OTSubscriber, createSession} from 'opentok-react';
 
 export default class ChatForm extends React.Component {
   state = {
     value: ''
-  }
-
-  componentWillMount() {
-
-    // This fetch will activate a lambda function which returns a sessionId and is then set in our state.
-    fetch('/.netlify/functions/tokbox')
-     .then(response => response.json())
-     .then(json => this.setState({sessionId: json.payload}));
-
-    this.sessionHelper = createSession({
-      apiKey: '46086882',
-      sessionId: '2_MX40NjA4Njg4Mn5-MTUyMTkyNjUwMjA2MX5FL1JpeDdubzFqVnhXMG0zOGV2cmUyTDZ-fg',
-      token: 'T1==cGFydG5lcl9pZD00NjA4Njg4MiZzaWc9Yzk4OTZiZDdmMDViMWNjNDViYjc1ZTk1YzU5MTYzMDE1YjU0YjBmYjpzZXNzaW9uX2lkPTJfTVg0ME5qQTROamc0TW41LU1UVXlNVGt5TmpVd01qQTJNWDVGTDFKcGVEZHViekZxVm5oWE1HMHpPR1YyY21VeVREWi1mZyZjcmVhdGVfdGltZT0xNTIxOTMxMzUxJm5vbmNlPTAuMjg4OTgwNzE3MDc3NDYyMSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTI0NTIzMzQ5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
-      onStreamsUpdated: streams => { this.setState({ streams }); }
-    });
-
-    this.properties = {
-      name: 'Bruce'
-    };
-
-    this.sessionHelper.session.on('signal:msg', function(event) {
-      console.log(event);
-    });
   }
 
   componentDidMount() {
@@ -61,18 +37,6 @@ export default class ChatForm extends React.Component {
       e.target.value = '';
       this.setState({ value: '' });
     }
-
-    this.sessionHelper.session.signal({
-      type: 'msg',
-      data: 'foo'
-    }, function(error) {
-    if (error) {
-      console.log('Error sending signal:', error.name, error.message);
-    } else {
-      
-    }
-
-  });
   }
 
   handleChange = (e) => {
--- src/components/Subscriber.js
+++ src/components/Subscriber.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { OTSession, OTPublisher, OTStreams, OTSubscriber, createSession} from 'opentok-react';
 
 class Subscriber extends React.Component {
 
@@ -9,43 +8,9 @@ class Subscriber extends React.Component {
 
   }
 
-  componentWillMount() {
-
-    // This fetch will activate a lambda function which returns a sessionId and is then set in our state.
-
-    fetch('/.netlify/functions/tokbox')
-     .then(response => response.json())
-     .then(json => this.setState({sessionId: json.payload}));
-
-
-    this.sessionHelper = createSession({
-      apiKey: '46086882',
-      sessionId: '2_MX40NjA4Njg4Mn5-MTUyMTkyNjUwMjA2MX5FL1JpeDdubzFqVnhXMG0zOGV2cmUyTDZ-fg',
-      token: 'T1==cGFydG5lcl9pZD00NjA4Njg4MiZzaWc9Yzk4OTZiZDdmMDViMWNjNDViYjc1ZTk1YzU5MTYzMDE1YjU0YjBmYjpzZXNzaW9uX2lkPTJfTVg0ME5qQTROamc0TW41LU1UVXlNVGt5TmpVd01qQTJNWDVGTDFKcGVEZHViekZxVm5oWE1HMHpPR1YyY21VeVREWi1mZyZjcmVhdGVfdGltZT0xNTIxOTMxMzUxJm5vbmNlPTAuMjg4OTgwNzE3MDc3NDYyMSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTI0NTIzMzQ5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
-      onStreamsUpdated: streams => { this.setState({ streams }); }
-    });
-
-    this.properties = {
-      name: 'Bruce'
-    };
-  }
-
-  componentWillUnmount() {
-    this.sessionHelper.disconnect();
-  }
-
   render() {
     return (
       <div>
-        {this.state.streams.map(stream => {
-          return (
-            <OTSubscriber
-              key={stream.id}
-              session={this.sessionHelper.session}
-              stream={stream}
-            />
-          );
-        })}
       </div>
     );
   }
