d740a1fe860357269a23701cbd423a0cc302f9e2
Bruce Wong <xenovia@Bruces-MacBook-Pro.local>
2018-03-24 21:54:00 -0700

    add redux for userId

--- src/App.js
+++ src/App.js
@@ -2,55 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-// Import Actions
+// Import Actions 
 import { fetchSpeaker } from './frontend/actions/speakers_actions';
+
+// Import React-Redux
+import { connect } from 'react-redux';
 
 // Import our components:
 import VideoBox from './components/VideoBox';
 import ChatBox from './components/ChatBox';
-// ------------
 
-// class LambdaDemo extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {loading: false, msg: null};
-//   }
+// Import OpenTok
+import opentok from 'opentok';
 
-//   componentDidMount() {
-//     // check if you are the host
-
-//     // check if stream is speaker1
-//     fetchSpeaker
-//   }
-
-//   handleClick = (e) => {
-//     e.preventDefault();
-
-//     this.setState({loading: true});
-//     fetch('/.netlify/functions/hello')
-//       .then(response => response.json())
-//       .then(json => this.setState({loading: false, msg: json.msg}));
-//   }
-
-
-
-//   render() {
-//     const {loading, msg} = this.state;
-
-//     return <p>
-//       <button onClick={this.handleClick}>{loading ? 'Loading...' : 'Call Lambda'}</button><br/>
-//       <span>{msg}</span>
-//     </p>
-//   }
-// }
+var myOpenTok = new opentok('46086882', '65c732f900b54d8b78184fe88def8230377a0761');
 
 class App extends Component {
 
   componentDidMount() {
+    const { fetchSpeaker } = this.props;
     // check if you are the host
 
+
+    var sessionId = '2_MX40NjA4Njg4Mn5-MTUyMTkyNjUwMjA2MX5FL1JpeDdubzFqVnhXMG0zOGV2cmUyTDZ-fg'
+    var tokenOptions = {};
+    tokenOptions.role = "publisher";
+    tokenOptions.data = "username=bob";
+
+    // Generate a token.
+    var token = myOpenTok.generateToken(sessionId, tokenOptions);
+    console.log(token);
+
+
     // check if stream is speaker1
-    fetchSpeaker
+    fetchSpeaker()
+    
   }
 
   render() {
@@ -65,6 +51,12 @@ class App extends Component {
   }
 }
 
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchSpeaker: () => { dispatch(fetchSpeaker()) }
+  };
+};
 
 
-export default App;
+
+export default connect(null, mapDispatchToProps)(App);
