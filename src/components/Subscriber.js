b2a630c78ffb3e04deaebf5da5a01a95a9a8a0ba
locphan2207 <locphan2207@gmail.com>
2018-03-25 14:57:43 -0700

    pull request

--- src/components/Subscriber.js
+++ src/components/Subscriber.js
@@ -15,7 +15,7 @@ class Subscriber extends React.Component {
       apiKey: '46086882',
       sessionId: '2_MX40NjA4Njg4Mn5-MTUyMTkyNjUwMjA2MX5FL1JpeDdubzFqVnhXMG0zOGV2cmUyTDZ-fg',
       token: 'T1==cGFydG5lcl9pZD00NjA4Njg4MiZzaWc9Yzk4OTZiZDdmMDViMWNjNDViYjc1ZTk1YzU5MTYzMDE1YjU0YjBmYjpzZXNzaW9uX2lkPTJfTVg0ME5qQTROamc0TW41LU1UVXlNVGt5TmpVd01qQTJNWDVGTDFKcGVEZHViekZxVm5oWE1HMHpPR1YyY21VeVREWi1mZyZjcmVhdGVfdGltZT0xNTIxOTMxMzUxJm5vbmNlPTAuMjg4OTgwNzE3MDc3NDYyMSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNTI0NTIzMzQ5JmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9',
-      onStreamsUpdated: streams => { console.log(streams);this.setState({ streams }); } // real time watching
+      onStreamsUpdated: streams => { console.log(streams);this.setState({ streams }); } // real time watching stream
     });
   }
 
